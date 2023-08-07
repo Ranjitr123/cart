d2e5370cd366ed7af39534da3cd5669e0ae41f05
Ranjit Rautaray <ranjit.r@terralogic.com>
2023-08-07 12:09:54 +0530

    redux store added

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+// src/store.js
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default store;
