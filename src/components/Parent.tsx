a73946966bea684cfa900df8ccbfadb75eb97e8d
Ranjit Rautaray <ranjit.r@terralogic.com>
2023-08-08 12:47:35 +0530

    some files added

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -7,47 +7,58 @@ function Parent() {
   const dispatch = useDispatch();
 
   //  let number = 121;
-  //  palindrome 
-  function palindrome(x:any){
-      
-   
-     let z =  +x.toString().split("").reverse().join("")
-      console.log(z === x)
+  //  palindrome
+  function palindrome(x: any) {
+    let z = +x.toString().split("").reverse().join("");
+    console.log(z === x);
 
-      // 121 = "121" = [1,2,1] = [1,2,1] = 121
+    // 121 = "121" = [1,2,1] = [1,2,1] = 121
   }
 
-  palindrome(223)
+  palindrome(223);
 
-  // fibonance number 
+  // fibonance number
 
-  const fib = function (y:any){
-    let arr = [0,1]
-    for(let i = 2 ; i<=y ; i++)
-    {
-        arr.push(arr[i-1] + arr[i-2])
+  const fib = function (y: any) {
+    let arr = [0, 1];
+    for (let i = 2; i <= y; i++) {
+      arr.push(arr[i - 1] + arr[i - 2]);
     }
-    console.log(arr)
+    console.log(arr);
 
-   return arr[y]
-  
+    return arr[y];
+  };
+
+  fib(5);
+
+  // anagram test
+
+  let anagramtest = (t: any, s: any) => {
+    t = t.split("").sort().join("");
+    s = s.split("").sort().join("");
+    console.log(t === s);
+    return t === s;
+  };
+
+  anagramtest("sachin", "nischan");
+
+  // addsum test 
+
+  let array = [4,6,-3,5,1]
+  let target = 2
+
+  let addsum = (val:any,finalval:any)=>{
+    for(let i = 0 ; i<= val.length; i++){
+          for(let j = i+1; j<= val.length;j++  ){
+            if(val[i] + val [j] === finalval){
+            
+              return [i,j]
+            }
+          }
+    }
   }
+    console.log(addsum([4,6,2,5,1],7))
 
-  fib(5)
-
-  
-  // anagram test 
- 
-  let anagramtest = (t:any,s:any)=>{
-    t = t.split("").sort().join("")
-     s = s.split("").sort().join("")
-     console.log(t===s)
-    return t === s
-  }
-
-  anagramtest("sachin","nischan")
-
-  
   return (
     <div>
       <h1>Counter: {count}</h1>
